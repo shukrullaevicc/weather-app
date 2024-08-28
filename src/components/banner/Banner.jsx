5f774eab9826dd24e7386b9e46b25ab7eff5c0cb
Shukrullayev Botirxo'ja <botirshukrullayev@gmail.com>
2024-08-28 15:00:49 +0500

    first commit

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,6 +2,27 @@ import React, { useEffect, useState } from "react";
 import Container from "../../utils";
 import { useSelector } from "react-redux";
 import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+// Register the necessary components, including the scale
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const defaultData = {
   location: {
