7b56ceff3235888f59f5d59341813a7abb49fe97
Shukrullayev Botirxo'ja <botirshukrullayev@gmail.com>
2024-09-02 23:29:58 +0500

    I fixed some issues in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,15 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "light" : "url('./src/images/Light.jpg')",
-        "night" : "url('./src/images/Night.jpeg')",
-        "pressure" : "url('./src/images/pressure.jpg')",
-        "compass" : "url('./src/images/compass.png')",
-        "arrow" : "url('./src/images/arrow.png')",
+        "light" : "url('/images/Light.jpg')",
+        "night" : "url('/images/Night.jpeg')",
+        "pressure" : "url('/images/pressure.jpg')",
+        "compass" : "url('/images/compass.png')",
+        "arrow" : "url('/images/arrow.png')",
         "linear-color" : "linear-gradient(180deg, rgba(171,0,255,1) 0%, rgba(172,85,183,1) 66%, rgba(209,0,204,1) 100%);"
       },
       bgSize:{
-        "full" : "90%",
+        "full" : "100%",
       }
     },
   },
